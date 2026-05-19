5d35a612a106606fc1452b35cd4caa94b7231db4
asthana-16 <abhi546.luk@gmail.com>
2026-05-19 15:18:55 +0530

    initial commit

---
+++ Basics/Datatypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as Newer Version 
+
+// alert(3+3) //this works in browser but not in node js
+
+console.log(3+3);
+console.log('abhinav')
+
+// number => 2 to power 53  (range)
+//bigint , use for extremely large number not generally uesed
+//string +> = ""
+//boolean => true/flase
+//null => standalone value (empty) also an object
+//undefined => value not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof "abhinav"); //to know datatype
+console.log(typeof null); //null having datatype as object
+
+
+
---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail =  "abhinav123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState; //if we only declare variable it will give undefined output;
+// accountId = 2 //we cannot modify const
+// its perfer to not use Var only use let and const
+accountEmail = "aa@aa.com"
+accountPassword = "212121"
+accountCity = "pune"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
